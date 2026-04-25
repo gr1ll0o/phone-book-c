c0598dbfd8258300c71ad624813292f0394c2586
gr1ll0o <thiagotobias.grillo@gmail.com>
2026-04-25 13:35:03 -0300

    update & fixes: final version 1.0

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 #define MAX 10
 char names[MAX][50];
 char phones[MAX][20];
-char clear_console[] = "clear";
+char clear_console[] = "clear"; // LINUX: CLEAR ; WINDOWS: CLS
 int count = 0;
 
 int main() {
@@ -14,7 +14,7 @@ int main() {
 
     int opt;
     do {
-        printf("-- AGENDA TELEFONICA --\n");
+        printf("--- AGENDA TELEFONICA ---\n");
         printf("1. Ver contactos\n");
         printf("2. Agregar contactos\n");
         printf("3. Modificar contactos\n");
@@ -26,7 +26,7 @@ int main() {
             case 1: // View contacts
                 char sel[10];
                 printf("--- VER CONTACTOS ---\n");
-                for (int i = 0;i<10;i++) {
+                for (int i = 0;i < MAX;i++) {
                     if (strlen(names[i]) == 0 && strlen(phones[i]) == 0) {
                         printf("Nro %d. [VACIO]\n", i+1);
                     }else{
@@ -35,15 +35,15 @@ int main() {
                 }
                 printf("\nDesea realizar otra operacion? (CualquierTecla: Si/0: No) ");
                 scanf("%s", sel);
-                if (strcmp(sel, "0") == 0) return 0;
+                if (strcmp(sel, "0") == 0) opt = 4;
                 system(clear_console);
-                break;
+            break;
             case 2: // Add contacts
                 if (count >= MAX) printf("No te queda espacio. Modifica uno.\n");
                 else{
                     printf("--- AGREGAR CONTACTOS ---\n");
                     printf("Nombre?: ");
-                    scanf("%s", names[count]);
+                    scanf(" %[^\n]", names[count]);
                     printf("Telefono?: ");
                     scanf("%s", phones[count]);
                     count++; // COUNTER TO MAX
@@ -55,10 +55,9 @@ int main() {
                 if (count == 0) printf("No tenes contactos.\n");
                 else{
                     int c = count+1;
-                    while (c > count)
-                    {
+                    do {
                         printf("--- MODIFICAR CONTACTOS ---\n");
-                        for (int i = 0; i < 10; i++) {
+                        for (int i = 0; i < MAX; i++) {
                             if (strlen(names[i]) != 0 && strlen(phones[i]) != 0) {
                                 printf("Nro %d. %s (%s)\n", i + 1, names[i], phones[i]);
                             }
@@ -66,27 +65,23 @@ int main() {
                         printf("\nIntroduzca el numero de contacto a editar (0 para salir): ");
                         scanf("%d", &c);
                         system(clear_console);
-                        if (c > count) printf("Numero invalido.\n");
-                    }
+                        if (c > count || c <= 0) printf("Numero invalido.\n");
+                    }while (c > count || c <= 0);
 
-                    if (c == 0) break;
                     printf("Nombre?: ");
-                    scanf("%s", names[c-1]);
+                    scanf(" %[^\n]", names[c-1]);
                     printf("Telefono?: ");
                     scanf("%s", phones[c-1]);
                     system(clear_console);
-                    printf("Contacto modificado.");
+                    printf("Contacto modificado.\n");
                 }
             break;
-            case 4: // Exit
-                printf("Saliendo... ");
-                return 0;
-            break;
             default:
-                printf("Operacion invalida.\n");
+                if (opt != 4) printf("Operacion invalida.\n");
             break;
         }
     }while(opt != 4);
 
+    printf("Saliendo...");
     return 0;
 }
